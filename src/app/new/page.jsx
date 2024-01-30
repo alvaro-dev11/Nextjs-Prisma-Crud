0b6e791af04b19f36d7decfb065eb0c145578bbc
Alvaro Antony Villanueva Barreto <barretoalvaro369@gmail.com>
2024-01-30 17:35:14 -0500

    Frontend: Crear Tarea

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+"use client";
+import { useRouter } from "next/navigation";
 
 const NewPage = () => {
-  return (
-    <div>
-      Formulario de tareas
-    </div>
-  )
-}
+  const router = useRouter();
 
-export default NewPage
+  const onSubmit = async (e) => {
+    // console.log(e)
+    e.preventDefault();
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+
+    const res = await fetch("/api/tasks", {
+      method: "POST",
+      body: JSON.stringify({ title, description }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+
+    router.push("/");
+  };
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <form onSubmit={onSubmit} className="bg-slate-800 p-10 lg:w-1/4 md:w-1/2">
+        <label htmlFor="title" className="font-bold text-sm mb-4 block">
+          Título de la tarea
+        </label>
+        <input
+          type="text"
+          id="title"
+          placeholder="Título"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+        />
+        <label htmlFor="description" className="font-bold text-sm">
+          Descripción de la tarea
+        </label>
+        <textarea
+          rows="3"
+          id="description"
+          placeholder="Describe tu tarea"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"></textarea>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Crear
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default NewPage;
