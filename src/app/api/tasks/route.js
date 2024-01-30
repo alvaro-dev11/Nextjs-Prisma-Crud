ef5e04c4eeaf9c5cc6c21da548bbb59d33e9d55e
Alvaro Antony Villanueva Barreto <barretoalvaro369@gmail.com>
2024-01-30 09:31:06 -0500

    Rutas API REST terminadas

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,6 +1,20 @@
 // Rutas de Backend: API
 import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
 
-export function GET(){
-    return NextResponse.json("Listando tareas")
+export async function GET() {
+  const tasks = await prisma.task.findMany();
+  // console.log(tasks);
+
+  return NextResponse.json(tasks);
+}
+export async function POST(request) {
+  const { title, description } = await request.json();
+  const newTask = await prisma.task.create({
+    data: {
+      title,
+      description,
+    },
+  });
+  return NextResponse.json(newTask);
 }
---
+++ src/libs/prisma.js
@@ -0,0 +1,3 @@
+// Conectando a la BD de Sqlite
+import { PrismaClient } from "@prisma/client";
+export const prisma = new PrismaClient();
